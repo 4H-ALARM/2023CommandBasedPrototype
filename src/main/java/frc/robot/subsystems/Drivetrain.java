e25833cb7c0db08e3f57c5d40be7b10c0c84a295
Shop <fkalmback@comcast.net>
2023-01-22 11:01:26 -0500

    first commit of framework

---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+public class Drivetrain extends SubsystemBase {
+  private final WPI_TalonFX m_frontLeft = new WPI_TalonFX(1);
+  private final WPI_TalonFX m_rearLeft = new WPI_TalonFX(2);
+  private final WPI_TalonFX m_frontRight = new WPI_TalonFX(3);
+  private final WPI_TalonFX m_rearRight = new WPI_TalonFX(4);
+
+  private final MecanumDrive m_drive =
+      new MecanumDrive(m_frontLeft, m_rearLeft, m_frontRight, m_rearRight);
+
+
+  /** Creates a new Drivetrain. */
+  public Drivetrain() {
+
+    
+  }
+
+  /**
+   * Drives the robot at given x, y and theta speeds. Speeds range from [-1, 1] and the linear
+   * speeds have no effect on the angular speed.
+   *
+   * @param xSpeed Speed of the robot in the x direction (forward/backwards).
+   * @param ySpeed Speed of the robot in the y direction (sideways).
+   * @param rot Angular rate of the robot.
+   * @param fieldRelative Whether the provided x and y speeds are relative to the field.
+   */
+  public void drive(double xSpeed, double ySpeed, double rot) {
+    m_drive.driveCartesian(ySpeed, xSpeed, rot);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Grabber.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+public class Grabber extends SubsystemBase {
+
+  private final WPI_VictorSPX m_clawmotor = new WPI_VictorSPX(4);
+
+  /** Creates a new Grabber. */
+  public Grabber() {
+    m_clawmotor.setInverted(false);
+  }
+
+  public void open(){
+    m_clawmotor.set(0.7);
+  }
+
+  public void close() {
+    m_clawmotor.set(-0.9);
+  }
+
+  public void stop() {
+    m_clawmotor.set(0.0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+}
