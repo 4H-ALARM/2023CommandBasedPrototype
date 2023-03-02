8eb4a4f4a8ee2055a97fd8c472c8d22530b85ec3
arnoldtkalmbach <fkalmbach@comcast.net>
2023-03-01 19:55:08 -0500

    Shuffleboard changes

--- src/main/java/frc/robot/subsystems/vision.java
+++ src/main/java/frc/robot/subsystems/vision.java
@@ -10,6 +10,7 @@ import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.Debug;
 
 
 public class vision extends SubsystemBase {
@@ -42,17 +43,25 @@ public class vision extends SubsystemBase {
     double area = m_ta.getDouble(0.0);
     
   
-
-    SmartDashboard.putNumber("LimelightX", x);
-    SmartDashboard.putNumber("LimelightY", y);
-    SmartDashboard.putNumber("LimelightArea", area);
-    SmartDashboard.putNumber("AprilTag ID;", id);
+    if (Debug.VisionON) {
+      SmartDashboard.putNumber("LimelightX", x);
+      SmartDashboard.putNumber("LimelightY", y);
+      SmartDashboard.putNumber("LimelightArea", area);
+      SmartDashboard.putNumber("AprilTag ID;", id);
+    }
+    
 
     double ledState = m_ledMode.getDouble(-1);
-    SmartDashboard.putNumber("ledMode", ledState);
+    if (Debug.VisionON) {
+      SmartDashboard.putNumber("ledMode", ledState);
+    }
+    
 
     m_pipeLine.setNumber(m_currentPipeline);
-    SmartDashboard.putNumber("Pipe", m_currentPipeline);
+    if (Debug.VisionON) {
+      SmartDashboard.putNumber("Pipe", m_currentPipeline);
+    }
+    
 
   }
 
