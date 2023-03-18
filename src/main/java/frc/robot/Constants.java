20870307f09b8dcb93ffe88b9b2bfffed3d0baa2
Shop <fkalmback@comcast.net>
2023-03-18 11:36:46 -0400

    debug drive to optimal DriveToOptimalTargetToRobotDistance

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -96,7 +96,7 @@ package frc.robot;
       //unreachable code
       public static final boolean DriveON = false;
       public static final boolean ArmON = false;
-      public static final boolean VisionON = false;
+      public static final boolean VisionON = true;
     }
 }
   
