429770ab7f64bb0cff1b3cf836a45d5ef937d1a1
Marzoman5 <aemarzoratti@gmail.com>
2023-03-30 21:16:27 -0400

    3/30 doneish

---
+++ src/main/java/frc/robot/commands/changeLedPurple.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Leds;
+
+public class changeLedPurple extends CommandBase {
+  private Leds m_Leds;
+  Integer m_counter = 0;
+
+  /** Creates a new changeLedcolor. */
+  public changeLedPurple(Leds l) {
+    this.m_Leds = l;
+
+    // Use addRequirements(v) here to declare subsystem dependencies.
+    addRequirements(l);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_Leds.setRGB(255,0,255);
+    m_Leds.ledColor = true;
+    m_Leds.updateDashboard();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
